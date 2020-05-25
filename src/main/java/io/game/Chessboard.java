c152ba5289e607333130dbf6530bbc5cb0f33fa7
Ashutosh Sharma <email2ashusharma@gmail.com>
2020-05-25 10:43:25 +0530

    added input parsers

--- src/main/java/io/game/Chessboard.java
+++ src/main/java/io/game/Chessboard.java
@@ -1,6 +1,7 @@
 package io.game;
 
 import io.game.pieces.Piece;
+import io.game.pieces.PieceType;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,7 +23,43 @@ public class Chessboard {
     }
 
     public static void process(String input) {
+        try {
+            parseInput(input);
+            findAllNextPositions(piece, position);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Not a valid input Position");
+        }
+    }
 
+
+    public static void parseInput(String input) throws IllegalArgumentException {
+        if (input != null && input.length() == 2) {
+            String[] arr = input.split(" ");
+            piece = parsePiece(arr[0]);
+            position = new Position(arr[1]);
+        } else {
+            throw new IllegalArgumentException("Invalid Input");
+        }
+
+
+    }
+
+    public static Piece parsePiece(String strPiece) throws IllegalArgumentException {
+        if (strPiece.equals("King")) {
+            return PieceType.KING.create();
+        } else if (strPiece.equals("Queen")) {
+            return PieceType.QUEEN.create();
+        } else if (strPiece.equals("Bishop")) {
+            return PieceType.BISHOP.create();
+        } else if (strPiece.equals("Horse")) {
+            return PieceType.HORSE.create();
+        } else if (strPiece.equals("Rook")) {
+            return PieceType.ROOK.create();
+        } else if (strPiece.equals("Pawn")) {
+            return PieceType.PAWN.create();
+        } else {
+            throw new IllegalArgumentException("Invalid Input");
+        }
     }
 
 
